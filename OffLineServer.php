4a97ffe715cdcb668efd4a7d7eaa6a0ed436b175
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2023-08-07 20:55:33 -0300

    fazendo o front da mensagem de erro de conexão do banco dados

--- OffLineServer.php
+++ OffLineServer.php
@@ -3,9 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Erro de conexão</title>
 </head>
+
+<style>
+    .quebra{
+        padding: 0px;
+        background-color:#000; 
+    }
+
+    .tamanho{
+        height: 300px;
+        ver
+    }
+    
+    .itens{
+        display: inline-block;
+        vertical-align: 
+    }
+    .alinha{
+        height: 100vh;
+        display: grid;
+        justify-content: center;
+        align-content: center;
+    }
+
+</style>
+
 <body>
-    <h1>Sem conexão com o MySQL Server</h1>
+    <div class="quebra alinha">
+        <div class="alert alert-danger tamanho" role="alert">
+            <div class="itens">
+                <svg xmlns="http://www.w3.org/2000/svg" width="90px" height="90px" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
+                    <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z"/>
+                </svg>
+            </div>
+            <div class="itens">
+                <h1>Sem conexão com o MySQL Server</h1>
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
